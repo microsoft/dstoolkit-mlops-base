bea7fb68654a8fd0ea06f6f7820a69696bec6b28
Liana Napalkova <liana.napalkova@gmail.com>
2021-02-15 11:47:50 +0000

    Updated register.py

--- src/register.py
+++ src/register.py
@@ -1,63 +1,103 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-from azureml.core import Run, Experiment, Workspace
-from azureml.core.model import Model as AMLModel
-import argparse
 import os
+import sys
 import joblib
-import traceback
+import argparse
+from azureml.core import Run
+from azureml.core.model import Model as AMLModel
+from azureml.core.run import _OfflineRun
+from utils import append_traceability_logs
 
 
 def parse_args(args=None):
     parser = argparse.ArgumentParser()
+
     parser.add_argument(
-        "--model_name",
+        '--model_path',
         type=str,
-        help="Name of the Model",
-        default="sale_regression.pkl",
+        help=('The input from previous steps')
     )
 
     parser.add_argument(
-        "--model_path",
+        '--model_file_name',
         type=str,
-        help=("input from previous steps")
+        help='The name of the model file',
+        default='sale_regression.pkl',
     )
+
+    parser.add_argument(
+        '--model_name',
+        type=str,
+        help='The name of the model',
+        default='sale_regression',
+    )
+
+    parser.add_argument(
+        '--model_description',
+        type=str,
+        help=('The description of the model')
+    )
+
     args_parsed = parser.parse_args(args)
     return args_parsed
 
-def main(model_name, model_path):
-    run = Run.get_context()
-    ws = run.experiment.workspace
-    print("Loading model from "+model_path)
-    model_file = os.path.join(model_path, model_name)
-    model = None
-    try:
-        # Load model
-        model = joblib.load(model_file)
-        parent_tags = run.parent.get_tags()
-        print("Parent tags")
-        print(parent_tags)
-    except Exception:
-        traceback.print_exc(limit=None, file=None, chain=None)
-        print("Something went wrong getting get model. Exiting")
-        raise
-    if model is not None:
-        try:
-            print("Register model")
-            #TODO: use register from run
-            model = AMLModel.register(model_path = model_path,
-                                        model_name = model_name,
-                                        tags = parent_tags,
-                                        description="Orange juice sale forecasting",
-                                        workspace = ws)
-        except Exception:
-            traceback.print_exc(limit=None, file=None, chain=None)
-            print("Something went wrong registering model. Exiting")
-            raise
 
-if __name__=="__main__":
+def run_registration(
+                        model_path,
+                        model_file_name,
+                        model_name,
+                        model_description
+    ):
+    """Register the model.
+
+    Args:
+        model_path (str): The path to the model file
+        model_file_name (str): The name of the model file
+        model_name (str): The name of the model
+        model_description (str): The description of the model
+
+    Returns:
+        None
+
+    """
+
+    try:
+        run = Run.get_context()
+    except Exception:
+        print('cannot get a run')
+
+    if not isinstance(run, _OfflineRun):
+        ws = run.experiment.workspace
+        parent_tags = run.parent.get_tags()
+        print("parent tags: {}".format(parent_tags))
+    else:
+        print('registration step is not supported for local runs')
+        sys.exit(-1)
+
+    try:
+        # Register model
+        model = AMLModel.register(
+                                    model_path=model_path,
+                                    model_name=model_name,
+                                    tags=parent_tags,
+                                    description=model_description,
+                                    workspace=ws
+        )
+        print('registered a new model {}'.format(model.name))
+    except Exception:
+        print('cannot register a model')
+        sys.exit(-1)
+
+
+if __name__ == '__main__':
     args = parse_args()
-    model_name = args.model_name
     model_path = args.model_path
-    main(model_name, model_path)
+    model_file_name = args.model_file_name
+    model_name = args.model_name
+    model_description = args.model_description
+
+    run_registration(
+                        model_path,
+                        model_file_name,
+                        model_name,
+                        model_description
+    )
