3f8bf1ee4d3978454f716fbe4c911cb5cf2e4617
Trang Nguyen <trang.nguyen@microsoft.com>
2021-02-18 05:23:37 +0100

    Notebook migration.  Add create_dataset

---
+++ src/create_dataset.py
@@ -0,0 +1,38 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import argparse
+
+from azureml.opendatasets import OjSalesSimulated
+
+
+def main(data_path, maxfiles=None):
+
+    # Pull the data
+    oj_sales_files = OjSalesSimulated.get_file_dataset()
+    if maxfiles:
+        oj_sales_files = oj_sales_files.take(maxfiles)
+
+    # Download the data
+    file_paths = oj_sales_files.download(data_path, overwrite=True)
+
+    return file_paths
+
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, required=True)
+    parser.add_argument('--maxfiles', type=int, default=10)
+    args_parsed = parser.parse_args(args)
+
+    args_parsed.maxfiles = None if args_parsed.maxfiles <= 0 else args_parsed.maxfiles
+
+    return args_parsed
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    file_paths = main(data_path=args.data_path, maxfiles=args.maxfiles)
+
+    print(f'{len(file_paths)} files downloaded into {args.data_path}')
