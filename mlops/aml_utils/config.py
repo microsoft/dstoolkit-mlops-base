f624abc4144a2135de2ce55f68ca810fb51bfa37
Maria Medina <maria.medina.contractor@pepsico.com>
2023-09-14 14:00:12 +0200

    Rename operations to mlops

--- mlops/aml_utils/config.py
+++ mlops/aml_utils/config.py
@@ -34,7 +34,7 @@ def get_env_var(name):
 
 
 def get_root_path():
-    return Path(__file__).parents[3]  # go 2 layer up
+    return Path(__file__).parents[2]  # go 2 layers up
 
 
 def retrieve_config():
